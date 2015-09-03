fbef7a7685250200ad6265830863047fcb83e584
Danai Adkisson <dadkisso@live.com>
2015-09-02 22:20:01 -0400

    BOOM

--- palindrome.py
+++ palindrome.py
@@ -1,28 +1,29 @@
 import re
-
-word = input("Enter a word and see if it is a palindrome\n")
+# blue = "Blue!"
+# print(blue)
+# blue = blue.lower()[::-1]
+# print(blue)
+# blue = re.sub(r'\W',"", blue)
+#
+# print(blue)
 def is_palindrome(user_string):
     # todo: return True or False if the sentence is or isn't a palindrome
-    user_string = user_string#.lower()[::-1]
-    match = re.search(r'[a-z A-Z]*', user_string)
-    return(user_string)
-    if user_string != match:
-        pass
+    user_string = user_string.lower()
+    user_string = re.sub(r'\W',"", user_string)
+    reverse_string = user_string[::-1]
+    print(reverse_string)
+    if user_string == reverse_string:
+        return True
+
+    else:
         return False
-    
-    return True
-    # pass
 
-print(is_palindrome(word))
+def main():
+    word = input("Enter a word and see if it is a palindrome\n")
+    if is_palindrome(word) == True:
+        return "Your Sentance is a palendrome"
+    else:
+        return "nope"
 
-
-
-
-# is
-# def main():
-#     # TODO: put your input/output code here
-#     pass
-#
-#
-#     if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
